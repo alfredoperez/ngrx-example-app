2f15a043ee1bfca669d7d8d9aa867f9d0500d11f
Alfredo Perez <alfredo.perez.q@gmail.com>
2022-07-11 11:17:10 -0500

    Updates workflow

--- src/app/entity-state-example/state/reducers/friends.reducers.ts
+++ src/app/entity-state-example/state/reducers/friends.reducers.ts
@@ -1,13 +1,15 @@
 import { createEntityAdapter, EntityAdapter } from '@ngrx/entity';
 import { createReducer, on } from '@ngrx/store';
-import { EntityStateModel, LoadingState } from '../../../shared/models/entity-state.model';
+import {
+  EntityStateModel,
+  LoadingState
+} from '../../../shared/models/entity-state.model';
 import { Friend } from '../../../shared/models/friends.model';
 import { FriendsApiActions, FriendsTrackerPageActions } from '../actions';
 
 export const featureKey = 'friends';
 
-export interface FriendsState extends EntityStateModel<Friend> {
-}
+export type FriendsState = EntityStateModel<Friend>;
 
 export const adapter: EntityAdapter<Friend> = createEntityAdapter<Friend>({
   sortComparer: false
@@ -19,41 +21,32 @@ export const initialState: FriendsState = adapter.getInitialState({
 
 export const reducer = createReducer(
   initialState,
-  on(FriendsTrackerPageActions.enter,
-    (state) => ({
+  on(FriendsTrackerPageActions.enter, (state) => ({
+    ...state,
+    callState: LoadingState.LOADING
+  })),
+  on(FriendsApiActions.loadSuccess, (state, { friends }) => {
+    return adapter.setAll(friends || [], {
       ...state,
-      callState: LoadingState.LOADING
-    })),
-  on(FriendsApiActions.loadSuccess,
-    (state, {friends}) => {
-      return adapter.setAll(friends || [], {
-        ...state,
-        callState: LoadingState.LOADED
-      });
-    }),
-  on(FriendsApiActions.loadFailure,
-    (state, {error}) => ({
+      callState: LoadingState.LOADED
+    });
+  }),
+  on(FriendsApiActions.loadFailure, (state, { error }) => ({
+    ...state,
+    callState: { errorMessage: error }
+  })),
+  on(FriendsTrackerPageActions.addFriend, (state) => ({
+    ...state,
+    callState: LoadingState.LOADING
+  })),
+  on(FriendsApiActions.addFriendSuccess, (state, { friend }) => {
+    return adapter.addOne(friend, {
       ...state,
-      callState: {errorMessage: error}
-    })),
-  on(FriendsTrackerPageActions.addFriend,
-    (state) => ({
-      ...state,
-      callState: LoadingState.LOADING
-    })),
-  on(FriendsApiActions.addFriendSuccess,
-    (state, {friend}) => {
-      return adapter.addOne(friend, {
-        ...state,
-        callState: LoadingState.LOADED
-      });
-    }),
-  on(FriendsApiActions.addFriendFailure,
-    (state, {error}) => ({
-      ...state,
-      callState: {errorMessage: error}
-    })),
+      callState: LoadingState.LOADED
+    });
+  }),
+  on(FriendsApiActions.addFriendFailure, (state, { error }) => ({
+    ...state,
+    callState: { errorMessage: error }
+  }))
 );
-
-
-
